e2b2be6e54d93bf9b414f8b61c2ead7d3bd56c40
Kaede Fujisaki <kaede@hexe.net>
2020-10-18 16:06:46 +0900

    use custom certbot

--- create.sh
+++ create.sh
@@ -11,9 +11,12 @@ function create() {
         -vvv \
         --agree-tos \
         --email psi@7io.org \
-        --manual \
-        --keep \
+        --non-interactive \
         --preferred-challenges dns-01 \
+        --dns-valuedomain \
+        --dns-valuedomain-propagation-seconds=90 \
+        --dns-valuedomain-credentials=/etc/certbot/valuedomain.ini \
+        --keep \
         "$@"
   if [ $? -eq 0 ]; then
     echo Success.
@@ -23,9 +26,12 @@ function create() {
   fi
 }
 
-create -d 'ledyba.org' -d '*.ledyba.org'
-create -d '7io.org' -d '*.7io.org'
 create -d 'hexe.net' -d '*.hexe.net'
+create -d '7io.org' -d '*.7io.org' -d '*.app.7io.org'
+create -d 'ledyba.org' -d '*.ledyba.org'
 create -d 'open-dokidokivisual.com' -d '*.open-dokidokivisual.com'
 create -d 'dokidokivisual.org' -d '*.dokidokivisual.org'
 create -d 'hexe.ink' -d '*.hexe.ink'
+
+sudo find data -type f -exec chmod 644 {} \;
+sudo find data -type d -exec chmod 755 {} \;
--- renewal.sh
+++ renewal.sh
@@ -5,19 +5,20 @@ echo [$(date)] Started.
 PORTA_ROOT="/opt/books/porta"
 
 echo -n "Renew: "
-RESULT="$(docker-compose run --rm certbot renew -vvv 2>&1)"
+docker-compose run --service-ports --rm certbot renew -vvv 2>&1
+
 if [ $? -eq 0 ]; then
   echo Success.
 else
   echo Failure.
-  echo "$RESULT"
+  exit 255
 fi
 
 echo -n "Reloading nginx: "
-RESULT="$(env --chdir="${PORTA_ROOT}" docker-compose exec web /usr/sbin/nginx -s reload 2>&1)"
+env --chdir="${PORTA_ROOT}" docker-compose exec web /usr/sbin/nginx -s reload 2>&1
 if [ $? -eq 0 ]; then
   echo Success.
 else
   echo Failure.
-  echo "$RESULT"
+  exit 255
 fi
